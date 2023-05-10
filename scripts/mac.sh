8eeca5a866ed3d781d03d630fcd911f8509f2d36
Malek Rezgui <m.rezgui@outlook.fr>
2023-05-10 15:29:05 +0200

    mac: add preferences

---
+++ scripts/mac.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Finder: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "show hidden files by default"
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+
+echo "Show Path bar in Finder"
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder"
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Keep folder on top"
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+echo "Set Dock position to left"
+defaults write com.apple.dock "orientation" -string "left"
+
+echo "Set Dock auto hide"
+defaults write com.apple.dock "autohide" -bool "true"
+
+echo "Hide 'recent applications' from dock"
+defaults write com.apple.dock show-recents -bool false
+
+echo "Only show active apps in dock"
+defaults write com.apple.dock static-only -bool true
+
+echo "Speed up dock show/hide"
+defaults write com.apple.dock autohide-delay -float 0;
+defaults write com.apple.dock autohide-time-modifier -int 0;
+
+echo "Update Apple developer utils"
+softwareupdate --all --install --force
+
+killall SystemUIServer
+killall Dock
+killall Finder
